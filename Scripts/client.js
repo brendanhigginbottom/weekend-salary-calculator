f853f3cf29f248b085cfc855cbe8b19b4d43ceac
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-02-19 15:59:22 -0600

    tested that submitForm function is grabbing values and converting salary to number type and added template tbody html for use in appending

--- Scripts/client.js
+++ Scripts/client.js
@@ -3,4 +3,11 @@ console.log('JS Sourced!');
 function submitForm(event) {
     event.preventDefault();
     console.log('test');
+    let firstName = document.querySelector('#firstName').value;
+    let lastName = document.querySelector('#lastName').value;
+    let employeeID = document.querySelector('#employeeID').value;
+    let title = document.querySelector('#title').value;
+    let salary =  Number(document.querySelector('#salary').value);
+
+    console.log(firstName, lastName, employeeID, title, salary);
 }
